4e24bdafb72f6c124dacfd56e8979c34632022fe
Caio <cpenhalver@gmail.com>
2017-03-12 00:32:04 -0300

    Feature: register student

---
+++ src/app/students/student.ts
@@ -0,0 +1,6 @@
+export class Student{
+  constructor(public name: string,
+              public address: string,
+              public rg: string,
+              public course: string){}
+}
---
+++ src/app/students/student_form.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Student } from './student';
+import { FormGroup }   from '@angular/forms';
+
+@Component({
+  moduleId: module.id,
+  selector: 'student-register',
+  templateUrl: 'student_form.component.html'
+})
+
+export class StudentForm{
+  
+  public studentsList: Array<Student>;
+
+  constructor(){
+    this.studentsList = new Array<Student>();
+  }
+
+  courses = [
+    'Arquitetura de Software', 
+    'Desenvolvimento de games',
+    'Desenvolvimento mobile'
+  ];
+
+  onSubmit(form: FormGroup) {
+    console.log(form.value);
+    this.studentsList.push(new Student(form.value.name, 
+                                  form.value.address, 
+                                  form.value.rg,
+                                  form.value.course));
+    console.log(this.studentsList);
+  }
+}
